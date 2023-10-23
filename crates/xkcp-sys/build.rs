a9673dd4b74cfaf4b33a0cd1a20ea5b856aa4380
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-10-23 21:44:13 +0200

    refactor: build script

--- crates/xkcp-sys/build.rs
+++ crates/xkcp-sys/build.rs
@@ -5,80 +5,14 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
+    // early exit for better error messages
     need_command("make", Some("need GNU make"));
     need_command("xsltproc", Some("install xsltproc or libxslt"));
-
-    let target_arch = env("CARGO_CFG_TARGET_ARCH");
-    let target_features = env_opt("CARGO_CFG_TARGET_FEATURE").unwrap_or_default();
-    let target_features = target_features.split(',').collect::<Vec<_>>();
-    eprintln!("target features: {target_features:?}");
-    let feature = |s: &str| target_features.iter().any(|&f| f == s);
-    let mut xkcp_target = match target_arch.as_str() {
-        "x86_64" => {
-            if feature("avx512f") {
-                "AVX512"
-            } else if feature("avx2") {
-                "AVX2"
-            } else if feature("avx") {
-                "AVX"
-            } else if feature("ssse3") {
-                "SSSE3"
-            } else {
-                "generic64"
-            }
-        }
-        "armv6" => "ARMv6",
-        "armv7a" => "ARMv7A",
-        "armv7" => "ARMv7M",
-        "aarch64" => "ARMv8A",
-        "avr" => "AVR8",
-        _ => "compact",
-    };
-    eprintln!("initial XKCP target: {xkcp_target:?}");
-
-    if cfg!(feature = "avr8") {
-        eprintln!("AVR8 XKCP target forced");
-        xkcp_target = "AVR8";
-    } else if cfg!(feature = "force-compact") {
-        eprintln!("compact XKCP target forced");
-        xkcp_target = "compact";
-    } else if cfg!(feature = "force-generic") || xkcp_target == "compact" {
-        let w = if cfg!(feature = "force-generic") {
-            "forced"
-        } else {
-            "selected"
-        };
-        eprintln!("generic XKCP target {w}");
-        xkcp_target = match env("CARGO_CFG_TARGET_POINTER_WIDTH").as_str() {
-            "64" => "generic64",
-            "32" => "generic32",
-            width => {
-                eprintln!("no specialized target for pointer_width={width:?}");
-                "compact"
-            }
-        };
-    }
-
-    if cfg!(feature = "generic-lc") && xkcp_target.starts_with("generic") {
-        eprintln!("generic-lc XKCP target");
-        xkcp_target = match xkcp_target {
-            "generic32" => "generic32lc",
-            "generic64" => "generic64lc",
-            _ => unreachable!("expected `generic{{32,64}}` target, got {xkcp_target:?}"),
-        };
-    }
-
-    let xkcp_target = if let Ok(target) = env_opt("XKCP_RS_TARGET") {
-        eprintln!("XKCP target overridden by environment variable");
-        target
-    } else {
-        xkcp_target.to_string()
-    };
 
     let root = PathBuf::from(env("CARGO_MANIFEST_DIR"));
     let out_dir = PathBuf::from(env("OUT_DIR"));
     let xkcp_from = root.join("XKCP");
-    rerun_if_changed(xkcp_from.to_str().unwrap());
+    rerun_if_changed(xkcp_from.display());
 
     // copy XKCP to OUT_DIR
     let xkcp = out_dir.join("XKCP");
@@ -87,21 +21,30 @@ fn main() {
     eprintln!("out dir {}", xkcp.display());
 
     // build
-    eprintln!("final XKCP target: {xkcp_target:?}");
+    let xkcp_target = get_xkcp_target();
+    eprintln!("XKCP target selected: {xkcp_target}");
     patch_makefiles(&xkcp);
+
     let build = cc::Build::new();
     let cc = build.get_compiler();
     let ar = build.get_archiver();
-    let status = Command::new("make")
+
+    let cc_env = cc.cc_env();
+    let mut cc_env = cc_env.as_os_str();
+    if cc_env.is_empty() {
+        cc_env = cc.path().as_os_str();
+    }
+
+    let mut make = Command::new("make");
+    let make = make
         .arg(format!("{xkcp_target}/libXKCP.a"))
         .current_dir(&xkcp)
-        .env("CC", cc.to_command().get_program())
+        .env("CC", cc_env)
         .env("CFLAGS", cc.cflags_env())
-        .env("AR", ar.get_program())
-        .status()
-        .unwrap();
+        .env("AR", ar.get_program());
+    let status = make.status().unwrap();
     if !status.success() {
-        panic!("make failed");
+        panic!("make command failed:\n{make:?}");
     }
 
     // link
@@ -155,17 +98,90 @@ fn main() {
         .unwrap();
 }
 
-fn rerun_if_changed(path: &str) {
+fn get_xkcp_target() -> &'static str {
+    // XKCP_RS_TARGET takes precedence over everything
+    if let Ok(target) = maybe_env("XKCP_RS_TARGET") {
+        eprintln!("XKCP target overridden by environment variable");
+        return target.leak();
+    }
+
+    if cfg!(feature = "avr8") {
+        eprintln!("AVR8 XKCP target forced");
+        return "AVR8";
+    }
+    if cfg!(feature = "force-compact") {
+        eprintln!("compact XKCP target forced");
+        return "compact";
+    }
+    if cfg!(feature = "force-generic") {
+        eprintln!("generic XKCP target forced");
+        return generic_xkcp_target();
+    }
+
+    let mut xkcp_target = xkcp_target_from_cargo();
+
+    if cfg!(feature = "generic-lc") && xkcp_target.starts_with("generic") {
+        eprintln!("generic-lc XKCP target");
+        xkcp_target = match xkcp_target {
+            "generic32" => "generic32lc",
+            "generic64" => "generic64lc",
+            _ => unreachable!("expected `generic{{32,64}}` target, got {xkcp_target:?}"),
+        };
+    }
+
+    xkcp_target
+}
+
+fn xkcp_target_from_cargo() -> &'static str {
+    let target_arch = env("CARGO_CFG_TARGET_ARCH");
+    let target_features = maybe_env("CARGO_CFG_TARGET_FEATURE").unwrap_or_default();
+    let target_features = target_features.split(',').collect::<Vec<_>>();
+    eprintln!("target features: {target_features:?}");
+    let feature = |s: &str| target_features.iter().any(|&f| f == s);
+    match target_arch.as_str() {
+        "x86_64" => {
+            if feature("avx512f") {
+                "AVX512"
+            } else if feature("avx2") {
+                "AVX2"
+            } else if feature("avx") {
+                "AVX"
+            } else if feature("ssse3") {
+                "SSSE3"
+            } else {
+                "generic64"
+            }
+        }
+        "armv6" => "ARMv6",
+        "armv7a" => "ARMv7A",
+        "armv7" => "ARMv7M",
+        "aarch64" => "ARMv8A",
+        "avr" => "AVR8",
+        _ => generic_xkcp_target(),
+    }
+}
+
+fn generic_xkcp_target() -> &'static str {
+    match env("CARGO_CFG_TARGET_POINTER_WIDTH").as_str() {
+        "64" => "generic64",
+        "32" => "generic32",
+        width => {
+            eprintln!("falling back to compact XKCP target for target_pointer_width={width:?}");
+            "compact"
+        }
+    }
+}
+
+fn rerun_if_changed(path: impl std::fmt::Display) {
     println!("cargo:rerun-if-changed={path}");
 }
 
 #[track_caller]
 fn env(s: &str) -> String {
-    env_opt(s).unwrap()
+    maybe_env(s).unwrap()
 }
 
-#[track_caller]
-fn env_opt(s: &str) -> Result<String, env::VarError> {
+fn maybe_env(s: &str) -> Result<String, env::VarError> {
     println!("cargo:rerun-if-env-changed={s}");
     env::var(s)
 }
@@ -210,6 +226,7 @@ fn patch_makefiles(xkcp: &Path) {
     let mut contents = fs::read_to_string(&low_level_build).unwrap();
 
     const TO_REMOVE: &[&str] = &[
+        "<gcc>-fomit-frame-pointer</gcc>",
         "<gcc>-O</gcc>",
         "<gcc>-O2</gcc>",
         "<gcc>-g0</gcc>",
